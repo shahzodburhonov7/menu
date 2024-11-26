f9239c2f11789249c826a87f55a8143b9ddfc537
muhriddinbek-off <developer@Muhriddins-MacBook-Pro.local>
2024-11-26 19:04:55 +0500

    akjdg

--- lib/features/foods/widget/food_card.dart
+++ lib/features/foods/widget/food_card.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:restaurants_menu/common/colors/app_colors.dart';
 import 'package:restaurants_menu/common/extensions/text_extensions.dart';
+import 'package:zoom_tap_animation/zoom_tap_animation.dart';
 
 class FoodCard extends StatelessWidget {
   const FoodCard({
@@ -21,98 +22,49 @@ class FoodCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      alignment: Alignment.center,
-      clipBehavior: Clip.none,
-      children: [
-        Card(
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(16.0),
-          ),
-          elevation: 1,
-          child: Padding(
-            padding: REdgeInsets.only(top: 50),
-            child: SizedBox(
-              height: 227.h,
-              width: 164.w,
-              child: Column(
-                children: [
-                  SizedBox(height: MediaQuery.of(context).size.height * 0.1),
-                  Padding(
-                    padding: REdgeInsets.only(left: 10),
-                    child: Column(
-                      children: [
-                        Row(
-                          children: [
-                            name.s(14.sp).c(AppColors.hintTextColor).w(400),
-                          ],
-                        ),
-                        SizedBox(height: 4.h),
-                        Row(
-                          children: [
-                            '$price UZS'.s(14.sp).w(600).c(AppColors.black),
-                          ],
-                        )
-                      ],
+    return ZoomTapAnimation(
+      onTap: onTap,
+      child: Card(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(16.0),
+        ),
+        elevation: 1,
+        child: Padding(
+          padding: REdgeInsets.all(8.0.h),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(
+                height: 140.h,
+                width: double.infinity,
+                child: DecoratedBox(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10.r),
+                    image: DecorationImage(
+                      image: NetworkImage(image),
+                      fit: BoxFit.cover,
                     ),
                   ),
-                ],
-              ),
-            ),
-          ),
-        ),
-        Positioned(
-          top: 10,
-          left: 0,
-          right: 0,
-          child: Center(
-            child: GestureDetector(
-              onTap: onTap,
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(8.0),
-                child:
-                    // CachedNetworkImage(
-                    //   placeholder: (context, url) => CircularProgressIndicator(),
-                    //   imageBuilder: (context, imageProvider) => ClipRRect(
-                    //     borderRadius: BorderRadius.circular(16.r),
-                    //     child: Image.network(
-                    //       height: 149.h,
-                    //       width: 148.w,
-                    //       image,
-                    //       fit: BoxFit.cover,
-                    //     ),
-                    //   ),
-                    //   errorWidget: (context, url, error) => Assets.images.add.svg(),
-                    //   imageUrl: image,
-                    // ),
-                    Image.network(
-                  image,
-                  height: 149.h,
-                  width: 148.w,
-                  fit: BoxFit.cover,
+                  child: Align(
+                    alignment: const Alignment(0.8, -0.8),
+                    child: ZoomTapAnimation(
+                      onTap: onTap,
+                      child: const CircleAvatar(
+                        radius: 16,
+                        backgroundColor: AppColors.white,
+                        child: Icon(Icons.add),
+                      ),
+                    ),
+                  ),
                 ),
               ),
-            ),
+              SizedBox(height: 10.h),
+              name.s(14.sp).c(AppColors.hintTextColor).w(400),
+              '$price UZS'.s(14.sp).w(600).c(AppColors.black),
+            ],
           ),
         ),
-        Positioned(
-          top: 20,
-          right: 30,
-          child: GestureDetector(
-            onTap: onTap,
-            child: CircleAvatar(
-              backgroundColor: Colors.white,
-              radius: 16,
-              child: loading
-                  ? const CircularProgressIndicator()
-                  : const Icon(
-                      Icons.add,
-                      color: Colors.black,
-                    ),
-            ),
-          ),
-        ),
-      ],
+      ),
     );
   }
 }
